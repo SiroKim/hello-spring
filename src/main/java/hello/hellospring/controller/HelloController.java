419b930080170f3cf30c6e1ac0146649f37c2986
SiroKim <3kofking@google.com>
2021-05-05 08:06:24 +0900

    test 검증

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,19 +4,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") //url:localhost:8888/hello
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-        return "hello";
+        return "hello"; //resources/templates/hello.html
     }
 
-    @GetMapping("hello-mvc")
+    @GetMapping("hello-mvc")    //url:localhost:8888/hello-mvc
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-template";
+        return "hello-template"; //resources/templates/hello-template.html
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //HTTP BODY부에 직접 넣어준다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //hello 객체 반환
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
--- src/main/java/hello/hellospring/controller/MemberRepository.java
+++ src/main/java/hello/hellospring/controller/MemberRepository.java
@@ -1,2 +1,18 @@
-package hello.hellospring.controller;public interface MemberRepository {
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findbyId(Long id);
+
+    Optional<Member> findbyName(String name);
+
+    List<Member> findAll();
+
 }
